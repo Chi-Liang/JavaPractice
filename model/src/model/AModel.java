373f3adf99cc3642f34e621f5e9456bcd238f434
Chi-Liang <klchib3@gmail.com>
2020-01-12 10:02:32 +0800

    model

---
+++ model/src/model/AModel.java
@@ -0,0 +1,21 @@
+package model;
+
+public class AModel {
+	
+	String chinsesName;
+	String englishName;
+	
+	public String getChinsesName() {
+		return chinsesName;
+	}
+	public void setChinsesName(String chinsesName) {
+		this.chinsesName = chinsesName;
+	}
+	public String getEnglishName() {
+		return englishName;
+	}
+	public void setEnglishName(String englishName) {
+		this.englishName = englishName;
+	}
+	
+}
