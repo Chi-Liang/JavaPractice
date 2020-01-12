50612337e9a200f15564dd0b0d8a5d0a4eb91d40
Chi-Liang <klchib3@gmail.com>
2020-01-12 10:04:47 +0800

    Java

---
+++ project2/src/test1/Aaa.java
@@ -0,0 +1,10 @@
+package test1;
+
+import org.apache.log4j.Logger;
+
+public class Aaa {
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Aaa.class);
+		logger.info("aaaaaaa");
+	}
+}
